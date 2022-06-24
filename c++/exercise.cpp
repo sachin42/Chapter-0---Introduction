d7afaf345f68994d0969d44c8780687e6476b38c
Sachin Kushawaha <54734235+sachin42@users.noreply.github.com>
2022-06-24 22:21:29 +0530

    complete c++

--- c++/exercise.cpp
+++ c++/exercise.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
-
+#include <fstream>
 using namespace std;
 
 int main() {
-  string greeting = "Hello";
-  cout << greeting;
-  return 0;
+string myText;
+
+// Read from the text file
+ifstream MyReadFile("filename.txt");
+
+// Use a while loop together with the getline() function to read the file line by line
+while (getline (MyReadFile, myText)) {
+  // Output the text from the file
+  cout << myText;
+}
+
+// Close the file
+MyReadFile.close();
 }
